56a44c74125fe517c3daf6da98a6407c6bb2fd81
makmour <3614806+makmour@users.noreply.github.com>
2025-12-05 00:31:18 +0200

    Bump version to 1.4.4 new

--- wp-hide-backed-notices.php
+++ wp-hide-backed-notices.php
@@ -34,7 +34,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('WP_HIDE_BACKED_NOTICES_VERSION', '1.4.1');
+define('WP_HIDE_BACKED_NOTICES_VERSION', '1.4.4');
 
 /**
  * The code that runs during plugin activation.
